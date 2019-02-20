44dc19974dceca2b9d1f18e1dc90f2e7a0d6ea6d
marcosantinho <marcoaugustosantinho@hotmail.com>
2019-02-19 22:32:35 -0300

    Criacao do Projeto

---
+++ src/main/java/com/hostfinder/HostfinderApplication.java
@@ -0,0 +1,13 @@
+package com.hostfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostfinderApplication.class, args);
+	}
+
+}
